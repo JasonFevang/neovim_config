50752bce78d48c4e770b9ea412e9aca29814f1fa
Jason Fevang <jasonfevang@gmail.com>
2022-10-22 03:19:12 -0700

    Add astrisk as nvim surround delimiter

--- lua/user/surround.lua
+++ lua/user/surround.lua
@@ -8,6 +8,10 @@ if not status_ok then
 end
 
 surround.setup({
-    -- Configuration here, or leave empty to use defaults
+    delimiters = {
+        pairs = {
+            ["*"] = {'*', '*'}
+        }
+    },
 })
 
